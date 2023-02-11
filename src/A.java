82979175754c99faca47c8038593daf1c87ea5b9
khanhTran <khanhtrantung190894@gmail.com>
2023-02-11 18:49:05 +0700

    nested class

--- src/A.java
+++ src/A.java
@@ -1,2 +1,30 @@
 public class A {
+    private int x = 10;
+    class B {
+        private int x = 20;
+        class C {
+            private int x = 0;
+            class D {
+                private int x = 30;
+                public void allPrinctX() {
+                    System.out.println(x); //30
+                    System.out.println(this.x); //30
+                    System.out.println(B.this.x); //20
+                    System.out.println(A.this.x); //10
+                }
+            }
+
+        }
+
+
+    }
+    public static void main(String[] args) {
+        A a = new A();
+        B b = a.new B();
+        B.C c= b.new C();
+        B.C.D d = c.new D();
+
+        d.allPrinctX();
+
+    }
 }
