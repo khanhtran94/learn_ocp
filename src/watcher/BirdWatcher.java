43256de735dd39dc21b24cf1d1cf7c2685200fd8
khanhTran <khanhtrantung190894@gmail.com>
2023-02-19 20:57:09 +0700

    fix

--- src/watcher/BirdWatcher.java
+++ src/watcher/BirdWatcher.java
@@ -2,7 +2,7 @@ package watcher;
 
 import bird.Touca;
 import bird.Touca.Beak;
-import static bird.Touca.Beak.;
+import static bird.Touca.Beak;
 
 public class BirdWatcher {
     Touca.Beak beak = new Touca.Beak();
